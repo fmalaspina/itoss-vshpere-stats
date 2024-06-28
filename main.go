dbb0e0acce436d1fe0980ffb896d07fb32fc9fc3
fmalaspina <f_malaspina@hotmail.com>
2024-06-28 15:13:37 -0300

    initial

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/vmware/govmomi/vim25/soap"
 	"github.com/vmware/govmomi/vim25/types"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -24,10 +25,11 @@ var entityFlag = flag.String("entity", "host", "Optional. Usage: -entity <host|v
 var contextFlag = flag.String("context", "status", "Optional. Usage: -context <status|config|metrics>")
 var entityNameFlag = flag.String("entityName", "all", "Optional. Usage: -entityname <host, vm or resource name>")
 var timeoutFlag = flag.Duration("timeout", 10*time.Second, "Optional. Usage: -timeout <timeout in duration Ex.: 10s (ms,h,m can be used as well)>")
-var intervalFlag = flag.Int("i", 20, "Interval ID")
+var intervalFlag = flag.Int("i", 0, "Optional. Usage: -i <interval id>")
 var metricsFlag = flag.String("metrics", "cpu.usage.average", "Optional. Usage: -metrics <cpu.usage.average, mem.usage.average>")
 var functionsFlag = flag.String("functions", "last", "Optional. Usage: -functions <min,max,avg,last>")
 var maxSamplesFlag = flag.Int("maxSamples", 1, "Optional. Usage: -maxSamples <number of samples>")
+var instanceFlag = flag.String("instance", "*", "Optional. Usage: -instance <instance name>")
 
 // NewClient creates a vim25.Client for use in the examples
 func NewClient(ctx context.Context) (*vim25.Client, error) {
@@ -116,10 +118,30 @@ func main() {
 			defer v.Destroy(ctx)
 			return GetHostsStatus(ctx, err, v)
 		} else if *entityFlag == "host" && *contextFlag == "metrics" {
-			if *metricsFlag == "" || *intervalFlag == 0 {
-				fmt.Fprint(os.Stdout, "You must specify metrics and interval.\n")
+			if *metricsFlag == "" {
+				fmt.Fprint(os.Stdout, "You must specify metrics to query.\n")
 				flag.Usage()
 				os.Exit(0)
+			}
+
+			if *metricsFlag != "" {
+				metrics := strings.Split(*metricsFlag, ",")
+				if len(metrics) > 1 && *instanceFlag != "*" {
+					fmt.Fprint(os.Stdout, "You must specify only one metric when using instance.\n")
+					flag.Usage()
+					os.Exit(0)
+				}
+			}
+			var functions []string
+			if *functionsFlag != "last" {
+				functions = strings.Split(*functionsFlag, ",")
+				for _, f := range functions {
+					if !strings.Contains("last,min,max,avg", f) {
+						fmt.Fprint(os.Stdout, "You must specify a valid function (avg,min,max,last).\n")
+						flag.Usage()
+						os.Exit(0)
+					}
+				}
 			}
 
 			v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"HostSystem"}, true)
@@ -127,16 +149,16 @@ func main() {
 				return err
 			}
 			defer v.Destroy(ctx)
-			return GetHostMetrics(ctx, err, v)
+			return GetHostMetrics(ctx, err, v, functions)
 		}
-		fmt.Fprint(os.Stdout, "Option not implemented. Set host staus or host metrics.\n")
+		fmt.Fprint(os.Stdout, "Option not implemented. Set host status or host metrics.\n")
 		flag.Usage()
 		os.Exit(0)
 		return nil
 	})
 }
 
-func GetHostMetrics(ctx context.Context, err error, v *view.ContainerView) error {
+func GetHostMetrics(ctx context.Context, err error, v *view.ContainerView, functions []string) error {
 
 	vmsRefs, err := v.Find(ctx, []string{"HostSystem"}, nil)
 	if err != nil {
@@ -163,7 +185,7 @@ func GetHostMetrics(ctx context.Context, err error, v *view.ContainerView) error
 	// Create PerfQuerySpec
 	spec := types.PerfQuerySpec{
 		MaxSample:  int32(*maxSamplesFlag),
-		MetricId:   []types.PerfMetricId{{Instance: "0,1"}},
+		MetricId:   []types.PerfMetricId{{Instance: *instanceFlag}},
 		IntervalId: int32(*intervalFlag),
 	}
 
@@ -194,8 +216,31 @@ func GetHostMetrics(ctx context.Context, err error, v *view.ContainerView) error
 			}
 
 			if len(v.Value) != 0 {
-				fmt.Printf("%s\t%s\t%s\t%s\t%s\n",
-					name, instance, v.Name, v.ValueCSV(), units)
+				values, err := parseCSV(v.ValueCSV())
+				if err != nil {
+					fmt.Fprint(os.Stderr, "Error parsing metric CSV values: ", err, "\n")
+					os.Exit(0)
+				}
+				fmt.Printf("entity=%s;name=%s;instance=%s;metric=%s",
+					name.Type, name.Value, instance, v.Name)
+
+				for _, function := range functions {
+					result, err := applyFunction(values, function)
+					if err != nil {
+						fmt.Fprint(os.Stderr, "Error applying function:", err, "\n")
+						continue
+					}
+					fmt.Printf(";%s=%.2f", function, result)
+				}
+				fmt.Printf(";units:%s", units)
+				if *instanceFlag != "*" {
+					fmt.Println()
+				} else {
+					if len(strings.Split(*metricsFlag, ",")) > 1 {
+						fmt.Print("|")
+					}
+				}
+
 			}
 		}
 	}
@@ -238,4 +283,49 @@ func GetHostsStatus(ctx context.Context, err error, v *view.ContainerView) error
 		os.Exit(0)
 	}
 	return nil
+}
+
+// Function to apply the specified operation
+func applyFunction(values []float64, function string) (float64, error) {
+	switch function {
+	case "avg":
+		sum := 0.0
+		for _, value := range values {
+			sum += value
+		}
+		return sum / float64(len(values)), nil
+	case "min":
+		min := values[0]
+		for _, value := range values {
+			if value < min {
+				min = value
+			}
+		}
+		return min, nil
+	case "max":
+		max := values[0]
+		for _, value := range values {
+			if value > max {
+				max = value
+			}
+		}
+		return max, nil
+	case "last":
+		return values[len(values)-1], nil
+	default:
+		return 0, fmt.Errorf("unknown function: %s", function)
+	}
+}
+
+func parseCSV(csv string) ([]float64, error) {
+	parts := strings.Split(csv, ",")
+	values := make([]float64, 0, len(parts))
+	for _, part := range parts {
+		value, err := strconv.ParseFloat(strings.TrimSpace(part), 64)
+		if err != nil {
+			return nil, err
+		}
+		values = append(values, value)
+	}
+	return values, nil
 }
